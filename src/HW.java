dc415558984e8f143a9c0d4c0783df0e83294035
Xopppek <xopppekest@gmail.com>
2024-11-26 03:47:56 +0300

    added histogram drawing

--- src/HW.java
+++ src/HW.java
@@ -1,6 +1,8 @@
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
+import java.util.Arrays;
 
 public class HW extends Lab5{
     static {
@@ -8,8 +10,11 @@ public class HW extends Lab5{
     }
 
     static String savePath = "src/results/hw/";
+    static String loadPath = "src/resources/hw/";
 
     public static void main(String[] args) {
+        Mat gojoImg = Imgcodecs.imread(loadPath + "gojo.png");
+
         Point2D[] points = {
                 new Point2D(10, 10),
                 new Point2D(20, 5),
@@ -26,11 +31,66 @@ public class HW extends Lab5{
             canvasBSpline.drawPoint(point, Canvas.Color.RED);
         }
 
+        var canvasYHist = getHistY(gojoImg, 1000, 600);
+        var canvasCbHist = getHistCb(gojoImg, 1000, 600);
+        var canvasCrHist = getHistCr(gojoImg, 1000, 600);
+
         Imgcodecs.imwrite(savePath + "bspline.png", canvasBSpline.getImage());
+        Imgcodecs.imwrite(savePath + "gojo_y_hist.png", canvasYHist.getImage());
+        Imgcodecs.imwrite(savePath + "gojo_cb_hist.png", canvasCbHist.getImage());
+        Imgcodecs.imwrite(savePath + "gojo_cr_hist.png", canvasCrHist.getImage());
+
 
         displayImage(canvasBSpline.getImage(), 4, "B-Spline");
+        displayImage(canvasYHist.getImage(), 1, "Y-Hist");
+        displayImage(canvasCbHist.getImage(), 1, "Cb-Hist");
+        displayImage(canvasCrHist.getImage(), 1, "Cr-Hist");
 
         HighGui.waitKey(0);
+    }
+
+    public static CanvasHW getHistY(Mat image, int width, int height) {
+        var canvas = new CanvasHW(width, height);
+        var counts = new int[256];
+        var bgr = new byte[3];
+        for (int x = 0; x < image.width(); x++) {
+            for (int y = 0; y < image.height(); y++) {
+                image.get(y, x, bgr);
+                counts[(int) ((bgr[0] + 128) * 0.114 + (bgr[1] + 128) * 0.587 + (bgr[2] + 128) * 0.299)]++;
+            }
+        }
+        canvas.drawHist(counts);
+        return canvas;
+    }
+
+    public static CanvasHW getHistCb(Mat image, int width, int height) {
+        var canvas = new CanvasHW(width, height);
+        var counts = new int[256];
+        var bgr = new byte[3];
+        for (int x = 0; x < image.width(); x++) {
+            for (int y = 0; y < image.height(); y++) {
+                image.get(y, x, bgr);
+                double Y = ((bgr[0] + 128) * 0.114 + (bgr[1] + 128) * 0.587 + (bgr[2] + 128) * 0.299);
+                counts[(int) (0.5 * (bgr[0] + 128 - Y) / (1 - 0.114) + 128)]++;
+            }
+        }
+        canvas.drawHist(counts);
+        return canvas;
+    }
+
+    public static CanvasHW getHistCr(Mat image, int width, int height) {
+        var canvas = new CanvasHW(width, height);
+        var counts = new int[256];
+        var bgr = new byte[3];
+        for (int x = 0; x < image.width(); x++) {
+            for (int y = 0; y < image.height(); y++) {
+                image.get(y, x, bgr);
+                double Y = ((bgr[0] + 128) * 0.114 + (bgr[1] + 128) * 0.587 + (bgr[2] + 128) * 0.299);
+                counts[(int) (0.5 * (bgr[2] + 128 - Y) / (1 - 0.299) + 128)]++;
+            }
+        }
+        canvas.drawHist(counts);
+        return canvas;
     }
 }
 
@@ -84,4 +144,22 @@ class CanvasHW extends CanvasLab5 {
         drawBSplineBezierCurveCubic(points[m - 2], points[m - 1], points[m - 1], points[m - 1], bgr);
     }
 
+    public void drawHist(int[] values) {
+        if (values == null || values.length <= 1)
+            throw new IllegalArgumentException();
+
+        double dx = getWidth() * 1.0 / values.length;
+        double dy = getHeight() * 1.0 / Arrays.stream(values).max().orElse(1);
+
+        for (int i = 0; i < values.length; i++) {
+            var column = new Polygon((int) Math.round((i + 1) * dx), getHeight() - 1,
+                                     (int) Math.round(i * dx), getHeight() - 1,
+                                     (int) Math.round(i * dx), (int) Math.round(getHeight() - values[i] * dy),
+                                     (int) Math.round((i + 1) * dx), (int) Math.round(getHeight() - values[i] * dy));
+            fillPolygonEOMode(column, new byte[] {(byte) (i * 255.0 / values.length),
+                                                  (byte) (255 - i * 255.0 / values.length),
+                                                  0});
+            drawPolygon(column, Color.BLACK);
+        }
+    }
 }
